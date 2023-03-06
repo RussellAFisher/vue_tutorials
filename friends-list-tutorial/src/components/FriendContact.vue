c57ddd6f776e7bd539b9c3cf68038dd4a16d764e
Russell F <russell.a.fisher@gmail.com>
2023-03-06 09:57:44 -0700

    Vue tutorial first push

---
+++ friends-list-tutorial/src/components/FriendContact.vue
@@ -0,0 +1,57 @@
+<template>
+  <li>
+    <h2>{{name}} {{isFavorite ? '(Fave)' : ''}}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{phone}}</li>
+      <li><strong>Email:</strong> {{email}}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  emits: ['toggle-favorite'],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    phone: {
+      type: String,
+      required: false,
+      default: '-',
+      validator: function(value) {
+        return value.length >= 10 && value.length <= 13;
+      }
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false
+    }
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit('toggle-favorite', this.id);
+    }
+  }
+};
+</script>
